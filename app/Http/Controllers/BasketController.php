9701ce9eebfa402403013dfafcd4d133f390420d
jasmina031015 <jasmina031015@gmail.com>
2022-02-28 17:07:03 +0300

    test

---
+++ app/Http/Controllers/BasketController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class BasketController extends Controller
+{
+    public function basket()
+    {
+        return view('basket');
+    }
+
+    public function basketPlace()
+    {
+        return view('order');
+    }
+
+    public function basketAdd($productId)
+    {
+        $orderId = session('orderId');
+        if (is_null($orderId)) {
+            $order = Order::create();
+            session(['orderId' => $order->id]);
+        }else {
+            $order = Order::find($orderId);
+        }
+        $order->products()->attach($productId);
+           return view('basket', compact('order'));
+        }
+
+}
